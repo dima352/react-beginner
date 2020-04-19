fd269cd1c8d8b6c040b4d72a7a2a30f2f65ef8f4
dima352 <d-sasha.97@mail.ru>
2020-04-19 18:13:41 +0400

    blalala

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Navbar.css';
+const Navbar = () => {
+    return  <nav className='nav'>
+    <div className='item'>
+      Profile
+    </div>
+    <div className='item'>
+      Messages
+    </div>
+    <div className='item'>
+      News
+    </div>
+    <div className='item'>
+      Listen to the music
+    </div>
+    <div className='item'>
+      Settings
+    </div>
+  </nav>
+}
+
+
+
+export default Navbar;
